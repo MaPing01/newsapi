e9425ac63a905f39b5895fbbe36c54ad25570d79
MaPing01 <pingpingma001@163.com>
2018-11-29 10:29:07 +0800

    note

--- artical/views.py
+++ artical/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
 
 # Create your views here.
+class CategoryViewset(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryStringViewset(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategoryStringSerializer
+
+
+class CategoryPrimaryKeyViewset(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategoryPrimaryKeySerializer
+
+
+class CategorySlugViewset(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySlugSerializer
+
+
+class ItemViewset(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
